61bcc1f3f3401661cb310d80d35672db37d1bf53
Mellon <amutuheiremellon3@gmail.com>
2025-10-22 11:24:17 +0300

    Fix Profile

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profileController');
 
-router.post('/', profileController.createProfile);
+router.post('/api/profiles', profileController.createProfile); 
 router.get('/api/profiles', profileController.getAllProfiles);
-router.get('/search', profileController.searchProfiles);
-router.get('/:id', profileController.getProfileById);
-router.put('/:id', profileController.updateProfile);
+router.get('/api/profiles/search', profileController.searchProfiles); 
+router.get('/api/profiles/:id', profileController.getProfileById);
+router.put('/api/profiles/:id', profileController.updateProfile);
 
 module.exports = router;
